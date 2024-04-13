98804c61bf9a7325391f8b09b13a3d6d997823bd
DOTEL0Y <95876398+DOTEL0Y@users.noreply.github.com>
2024-04-13 15:33:44 -0400

    Add files via upload

---
+++ SVC.py
@@ -0,0 +1,24 @@
+import sklearn.svm as svm
+from sklearn.metrics import accuracy_score 
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+import seaborn as sns
+import matplotlib as plt
+allData = pd.read_csv("napchc13.csv")
+dataY = allData[['Treatment']].values
+dataX = allData.drop(['Treatment','Queen'], axis=1).values
+
+xTest, xTrain, yTest, yTrain = train_test_split(dataX, dataY.flatten(), train_size=0.5)
+classifier = svm.LinearSVC(penalty='l2', dual='auto')
+model = classifier.fit(xTrain,yTrain)
+model = model.predict(xTest)
+
+graph = sns.catplot(
+    data=allData, 
+    kind='swarm', x='Treatment', y='Queen', hue='c21'
+
+)
+graph.savefig('c21.png')
