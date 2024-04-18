10627ca26160e30f1209de9700bb443dcbb39ab4
DOTEL0Y <95876398+DOTEL0Y@users.noreply.github.com>
2024-04-17 22:00:22 -0400

    Update Scikit00.py
    Testing Augmented Data and slight changes to variable declaration

--- Scikit00.py
+++ Scikit00.py
@@ -9,13 +9,13 @@ import numpy as np
 #ADABOOST algorithm implementation
 from sklearn.ensemble import AdaBoostClassifier
 #ADABOOST Regressor - singular Training for Chemical molecule
-from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingClassifier
 #Splits Data from Train/Test Data
 from sklearn.model_selection import train_test_split
 
 #Establish STUMP for Classifier WEIGHT recalibration 
 from sklearn.tree import DecisionTreeClassifier
-
+#Accuracy Score Metric for Classifier
 from sklearn.metrics import accuracy_score
 
 
@@ -26,10 +26,10 @@ ada = AdaBoostClassifier(DecisionTreeClassifier(max_leaf_nodes=40,max_depth=24),
 #DecisionTreeClassifier -- MAX 40 leaf Nodes
 dtc = DecisionTreeClassifier(max_leaf_nodes=40,max_depth=24)
 
-carboClassifier = AdaBoostClassifier(n_estimators=25, algorithm="SAMME")
+gbc = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1)
 
 #csv File into allData Variable  ---> Later modify for TRAINING
-allData = pd.read_csv("napchc13.csv")
+allData = pd.read_csv("newNapchc13.csv")
 
 #Input 'data' var for spliting to TRAINING 
 data = allData
@@ -38,48 +38,15 @@ data = allData
 dataY = data[['Treatment']]
 
 #Data from Curticular Hydrocarbon
-dataX = data.drop(["Queen","Treatment"], axis=1).values
+dataX = data.drop(["Treatment"], axis=1).values
+#print(dataX,dataY)
 
 chc23_1 = data[["c23:1"]]
 
-# Split dataset into training set and test set
-X_train, X_test, Y_train, Y_test = train_test_split(dataX, dataY, test_size=0.5) # 50% training and 50% test
-ctest, ctrain = train_test_split(chc23_1, test_size=0.5)
-#convert Dataframe to NUM Array
-Y_train = Y_train.to_numpy()
-Y_test = Y_test.to_numpy()
-
-
-
-#train = train.reshape(1,20) // ALSO ---> Trains ML Algor
-ada.fit(X_train,Y_train.flatten())
-dtc.fit(X_train,Y_train.flatten())
-carboClassifier.fit(ctrain, Y_train.flatten())
-
-x1 = ada.predict(X_test)
-y1 = dtc.predict(X_test)
-x2 = carboClassifier.predict(ctest)
-
-accuracyX1 = accuracy_score(Y_test.flatten(), x1)
-accuracyY1 = accuracy_score(Y_test.flatten(), y1)
-accuracyX2 = accuracy_score(Y_test.flatten(), x2)
-#3D Figure 
-ax = plt.axes(projection="3d")
-
-ax.set_xlim(0,4)
-ax.set_xlabel("Predicted Treatment")
-
-ax.set_ylim(0,1)
-ax.set_ylabel("Accuracy Score")
-
-ax.set_zlabel("Actual Treatment")
-ax.set_zlim(0,4)
-
-#Converts String to INT for plotting 
 def reshapeTreatment(x):
         
         lst = []
-        for _ in range(len(x1)):
+        for _ in range(len(x)):
 
             if x[_] == 'C':
                 lst.append([0])
@@ -94,11 +61,45 @@ def reshapeTreatment(x):
         return lst
 
 
-ax.scatter(reshapeTreatment(x1),accuracyX1,reshapeTreatment(Y_test.flatten()), label="AdaBoost")
+dataY = np.array(dataY)
+# Split dataset into training set and test set
+X_train, X_test, Y_train, Y_test = train_test_split(dataX, dataY, test_size=0.25) # 50% training and 50% test
 
-ax.scatter(reshapeTreatment(y1),accuracyY1, reshapeTreatment(Y_test.flatten()), label="Decision Tree Classifier")
+ctest, ctrain = train_test_split(chc23_1, test_size=0.5)
 
-ax.scatter(reshapeTreatment(x2), accuracyX2, reshapeTreatment(Y_test.flatten()), label="AdaBoost 2")
+
+#Training of Data/Prediction
+adaboost = ada.fit(X_train,Y_train.flatten()).predict(X_test)
+decTree = dtc.fit(X_train,Y_train.flatten()).predict(X_test)
+gradBo = gbc.fit(X_train,Y_train.flatten()).predict(X_test)
+
+
+accuracyadaboost = accuracy_score(Y_test.flatten(), adaboost)
+accuracydecTree = accuracy_score(Y_test.flatten(), decTree)
+accuracygradBoo = accuracy_score(Y_test.flatten(),gradBo)
+
+
+#3D Figure 
+ax = plt.axes(projection="3d")
+ax.set_xlim(0,4)
+ax.set_xlabel("Predicted Treatment")
+
+ax.set_ylim(0,2)
+ax.set_ylabel("Accuracy Score")
+
+ax.set_zlabel("Actual Treatment")
+ax.set_zlim(0,4)
+
+#Converts String to INT for plotting 
+adaboost = reshapeTreatment(adaboost)
+decTree = reshapeTreatment(decTree)
+Y_test = reshapeTreatment(Y_test.flatten())
+gradBo = reshapeTreatment(gradBo)
+
+ax.scatter(adaboost,accuracyadaboost,Y_test, label="AdaBoost")
+ax.scatter(decTree,accuracydecTree, Y_test, label="Decision Tree Classifier")
+
+ax.scatter(gradBo, accuracygradBoo,Y_test, label="Graduate Boosting Classifier")
+
 plt.legend()
 plt.show()
-
